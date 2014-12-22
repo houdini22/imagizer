9f7bb58367e88c55358e68bd8ace4a3f6ad2eee4
Michal Baniowski <michal.baniowski@gmail.com>
2014-12-22 02:01:16 +0100

    Pinch effect

--- src/imagizer.js
+++ src/imagizer.js
@@ -2809,6 +2809,61 @@
         }
     });
 
+    Effects.defineTransform("pinch", function(x, y, parameters)
+    {
+        var dx = x - this.data.icentreX,
+            dy = y - this.data.icentreY,
+            distance = dx * dx + dy * dy,
+            d, t, e, a, s, c;
+
+        if (distance > this.data.radius2 || distance === 0)
+        {
+            return [x, y];
+        }
+        d = Math.sqrt(distance / this.data.radius2);
+        t = Math.pow(Math.sin(Math.PI * 0.5 * d), - parameters.amount);
+
+        dx *= t;
+        dy *= t;
+
+        e = 1 - d;
+        a = parameters.angle * e * e;
+
+        s = Math.sin(a);
+        c = Math.cos(a);
+
+        return [
+            this.data.icentreX + c * dx - s * dy,
+            this.data.icentreY + s * dx + c * dy
+        ];
+    }, {
+        defaults: {
+            angle: 0,
+            centreX: 0.5,
+            centreY: 0.5,
+            radius: 100,
+            amount: 0.5
+        },
+        before: function(parameters, width, height)
+        {
+            var icentreX = width * parameters.centreX,
+                icentreY = height * parameters.centreY,
+                radius = parameters.radius,
+                radius2;
+            if (radius === 0)
+            {
+                radius = Math.min(icentreX, icentreY);
+            }
+            radius2 = radius * radius;
+            return {
+                icentreX: icentreX,
+                icentreY: icentreY,
+                radius: radius,
+                radius2: radius2
+            };
+        }
+    });
+
     win.Imagizer.Project = Project;
     win.Imagizer.Layer = Layer;
     win.Imagizer.Image = ImageObj;
