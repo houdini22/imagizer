5d00fa000ed5616a65113f307c914e49955344f9
Michał Baniowski <baniczek@gmail.com>
2015-11-04 17:55:02 +0100

    effects: auto-contrast, auto-white-balance

--- src/imagizer.js
+++ src/imagizer.js
@@ -286,6 +286,174 @@
                 {
                     return new Array(7 - h.length).join("0") + h
                 })(bin.toString(16).toUpperCase())
+            },
+            RGBtoXYZ: function(r, g, b)
+            {
+                var var_R = ( r / 255 );
+                var var_G = ( g / 255 );
+                var var_B = ( b / 255 );
+
+                if(var_R > 0.04045)
+                {
+                    var_R = Math.pow(( var_R + 0.055 ) / 1.055, 2.4);
+                }
+                else
+                {
+                    var_R = var_R / 12.92;
+                }
+
+                if(var_G > 0.04045)
+                {
+                    var_G = Math.pow(( var_G + 0.055 ) / 1.055, 2.4);
+                }
+                else
+                {
+                    var_G = var_G / 12.92;
+                }
+
+                if(var_B > 0.04045)
+                {
+                    var_B = Math.pow(( var_B + 0.055 ) / 1.055, 2.4);
+                }
+                else
+                {
+                    var_B = var_B / 12.92;
+                }
+
+                var_R = var_R * 100;
+                var_G = var_G * 100;
+                var_B = var_B * 100;
+
+                return {
+                    x: var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805,
+                    y: var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722,
+                    z: var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505
+                };
+            },
+            RGBtoCIELab: function(r, g, b)
+            {
+                var xyz = helpers.color.RGBtoXYZ(r, g, b);
+
+                var var_X = xyz.x / 95.047; // ref
+                var var_Y = xyz.y / 100; // ref
+                var var_Z = xyz.z / 108.883; // ref
+
+                if(var_X > 0.008856)
+                {
+                    var_X = Math.pow(var_X, ( 1 / 3 ));
+                }
+                else
+                {
+                    var_X = ( 7.787 * var_X ) + ( 16 / 116 );
+                }
+
+                if(var_Y > 0.008856)
+                {
+                    var_Y = Math.pow(var_Y, ( 1 / 3 ));
+                }
+                else
+                {
+                    var_Y = ( 7.787 * var_Y ) + ( 16 / 116 );
+                }
+
+                if(var_Z > 0.008856)
+                {
+                    var_Z = Math.pow(var_Z, ( 1 / 3 ));
+                }
+                else
+                {
+                    var_Z = ( 7.787 * var_Z ) + ( 16 / 116 );
+                }
+
+                return {
+                    l: ( 116 * var_Y ) - 16,
+                    a: 500 * ( var_X - var_Y ),
+                    b: 200 * ( var_Y - var_Z )
+                };
+            },
+            CIELabToXYZ: function(l, a, b)
+            {
+                var var_Y = ( l + 16 ) / 116;
+                var var_X = a / 500 + var_Y;
+                var var_Z = var_Y - b / 200;
+
+                if(Math.pow(var_Y, 3) > 0.008856)
+                {
+                    var_Y = Math.pow(var_Y, 3);
+                }
+                else
+                {
+                    var_Y = ( var_Y - 16 / 116 ) / 7.787;
+                }
+
+                if(Math.pow(var_X, 3) > 0.008856)
+                {
+                    var_X = Math.pow(var_X, 3);
+                }
+                else
+                {
+                    var_X = ( var_X - 16 / 116 ) / 7.787;
+                }
+
+                if(Math.pow(var_Z, 3) > 0.008856)
+                {
+                    var_Z = Math.pow(var_Z, 3);
+                }
+                else
+                {
+                    var_Z = ( var_Z - 16 / 116 ) / 7.787;
+                }
+
+                return {
+                    x: 95.047 * var_X, // ref
+                    y: 100 * var_Y, // ref
+                    z: 108.883 * var_Z // ref
+                };
+            },
+            CIELabToRGB: function(l, a, b)
+            {
+                var xyz = helpers.color.CIELabToXYZ(l, a, b);
+
+                var var_X = xyz.x / 100;
+                var var_Y = xyz.y / 100;
+                var var_Z = xyz.z / 100;
+
+                var var_R = var_X * 3.2406 + var_Y * -1.5372 + var_Z * -0.4986;
+                var var_G = var_X * -0.9689 + var_Y * 1.8758 + var_Z * 0.0415;
+                var var_B = var_X * 0.0557 + var_Y * -0.2040 + var_Z * 1.0570;
+
+                if(var_R > 0.0031308)
+                {
+                    var_R = 1.055 * ( Math.pow(var_R, ( 1 / 2.4 )) ) - 0.055;
+                }
+                else
+                {
+                    var_R = 12.92 * var_R;
+                }
+
+                if(var_G > 0.0031308)
+                {
+                    var_G = 1.055 * ( Math.pow(var_G, ( 1 / 2.4 )) ) - 0.055;
+                }
+                else
+                {
+                    var_G = 12.92 * var_G;
+                }
+
+                if(var_B > 0.0031308)
+                {
+                    var_B = 1.055 * ( Math.pow(var_B, ( 1 / 2.4 )) ) - 0.055;
+                }
+                else
+                {
+                    var_B = 12.92 * var_B;
+                }
+
+                return {
+                    r: var_R * 255,
+                    g: var_G * 255,
+                    b: var_B * 255
+                };
             }
         },
         /**
@@ -4800,6 +4968,99 @@
             result.I = result.a11 * result.a22 - result.a21 * result.a12;
 
             return result;
+        }
+    });
+
+    Effects.defineCustom("auto-contrast", function(width, height)
+    {
+        var x, y,
+            min = Infinity, max = -1,
+            pixel,
+            remap = function remap(value)
+            {
+                return ((value) - min) * 255 / (max - min);
+            };
+
+        for(y = 0; y < height; y += 1)
+        {
+            for(x = 0; x < width; x += 1)
+            {
+                pixel = this.getPixel(x, y);
+
+                if((pixel.r + pixel.g + pixel.b) / 3 < min)
+                {
+                    min = (pixel.r + pixel.g + pixel.b) / 3;
+                }
+
+                if((pixel.r + pixel.g + pixel.b) / 3 > max)
+                {
+                    max = (pixel.r + pixel.g + pixel.b) / 3;
+                }
+            }
+        }
+
+        for(y = 0; y < height; y += 1)
+        {
+            for(x = 0; x < width; x += 1)
+            {
+                pixel = this.getPixel(x, y);
+
+                this.setPixel(x, y, {
+                    r: remap(pixel.r),
+                    g: remap(pixel.g),
+                    b: remap(pixel.b),
+                    a: pixel.a
+                });
+            }
+        }
+    });
+
+    Effects.defineCustom("auto-white-balance", function(width, height, parameters)
+    {
+        var x, y,
+            sumA = 0, sumB = 0,
+            pixel,
+            lab,
+            avgSumA, avgSumB,
+            aDelta, bDelta;
+
+        for(y = 0; y < height; y += 1)
+        {
+            for(x = 0; x < width; x += 1)
+            {
+                pixel = this.getPixel(x, y);
+                lab = helpers.color.RGBtoCIELab(pixel.r, pixel.g, pixel.b);
+                sumA += lab.a;
+                sumB += lab.b;
+            }
+        }
+
+        avgSumA = 0 - (sumA / (width * height));
+        avgSumB = 0 - (sumB / (width * height));
+
+        aDelta = avgSumA * (parameters.intensity / 100) * 1.1;
+        bDelta = avgSumB * (parameters.intensity / 100) * 1.1;
+
+        for(y = 0; y < height; y += 1)
+        {
+            for(x = 0; x < width; x += 1)
+            {
+                pixel = this.getPixel(x, y);
+
+                lab = helpers.color.RGBtoCIELab(pixel.r, pixel.g, pixel.b);
+
+                lab.a += aDelta;
+                lab.b += bDelta;
+
+                pixel = helpers.color.CIELabToRGB(lab.l, lab.a, lab.b);
+                pixel.a = this.getPixel(x, y).a;
+
+                this.setPixel(x, y, pixel);
+            }
+        }
+    }, {
+        defaults: {
+            intensity: 50
         }
     });
 
