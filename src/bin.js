7d854233dd93f98aede09352b6c4edc91982126a
houdini22 <baniczek@gmail.com>
2021-08-29 05:41:26 +0200

    updated binary

--- src/bin.js
+++ src/bin.js
@@ -9,17 +9,19 @@ const argv = yargs(hideBin(process.argv))
   .alias('help', 'h')
   .argv
 
-if (argv.effect === "EnchanceColors") {
-  const image = new Imagizer.Image();
-  image.load(argv.input, function () {
-    const project = new Imagizer.Project(image.getWidth(), image.getHeight());
-    const layer1 = project.createLayer();
-    const obj = layer1.put(image, 0, 0);
-    obj.applyEffect('component-stretching')
-    obj.applyEffect('auto-white-balance')
-    obj.applyEffect('auto-contrast')
-    project.save(argv.output, argv.imageType);
+if (argv.input && argv.output && argv.imageType) {
+  if (argv.effect === "EnchanceColors") {
+    const image = new Imagizer.Image();
+    image.load(argv.input, function () {
+      const project = new Imagizer.Project(image.getWidth(), image.getHeight());
+      const layer1 = project.createLayer();
+      const obj = layer1.put(image, 0, 0);
+      obj.applyEffect('component-stretching')
+      obj.applyEffect('auto-white-balance')
+      obj.applyEffect('auto-contrast')
+      project.save(argv.output, argv.imageType);
 
-    console.log(`Saved to ${argv.output}.`);
-  });
+      console.log(`Saved to ${argv.output} as ${argv.imageType}.`);
+    });
+  }
 }
